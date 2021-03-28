14b1ba534e60205bab3755ac7ede5b0be345a689
jesperkha <hammerjesp@gmail.com>
2021-03-29 00:48:46 +0200

    28.03.21

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 This new and improved version contain more options and custimization for requesting random meals. There are also a ton of qualtiy of life changes as well as improvements to the UI and UX.
 
-#### How it works
+### New features
+
+- More selections for food ethnicity
+- More selections for meal types
+- More extra options
--- client/requests.js
+++ client/requests.js
@@ -13,8 +13,42 @@ function getTags() {
 		if (e?.localName == "input" && e.checked) tags.push(e.value);
 	});
 
-	console.log(tags);
 	return tags;
+}
+
+function getValues(json) {
+	let data = {};
+
+	let recipe;
+	if (json.hasOwnProperty("instructions")) recipe = json;
+	else recipe = json.recipes[0];
+
+	data.instructions = [];
+	for (let i of recipe.instructions) {
+		data.instructions.push(i.display_text);
+	}
+
+	data.ingredients = [];
+	const ingredients = recipe.sections[0].components;
+	for (let i of ingredients) {
+		data.ingredients.push(i.raw_text);
+	}
+
+	data.name = json.name;
+	data.img = json.thumbnail_url;
+
+	const time = recipe.total_time_minutes;
+	const hr = Math.floor(time / 60);
+	const mn = time % 60;
+	if (hr > 0) data.time = `${hr} Hours ${mn} Minutes`;
+	else data.time = `${mn} Minutes`;
+
+	data.nutrition = recipe.nutrition;
+	data.servings = recipe.yields;
+
+	delete data.nutrition.updated_at;
+
+	return data;
 }
 
 async function requestRecipe() {
@@ -28,5 +62,7 @@ async function requestRecipe() {
 
 	const res = await fetch("/getRecipe", options);
 	const json = await res.json();
-	console.log(json);
+
+	localStorage.setItem("data", JSON.stringify(getValues(json.body)));
+	window.location = "./recipe.html";
 }
